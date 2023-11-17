36b2d21b6bd545fb2f7827916bb1d28c3b477524
devika-coder <devikas887@gmail.com>
2023-11-17 14:14:26 +0530

    commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var itemList = document.querySelector('#items');
+itemList.parentNode.style.backgroundColor='#7071E8'
+
+itemList.children[1].style.backgroundColor='#C5E898'
+
+itemList.firstElementChild.textContent='Hello'
